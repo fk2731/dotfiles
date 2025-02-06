010d30cdcfbb73db944272b054ef401024b6ac9d
fk2731 <leo.fk2731@gmail.com>
2025-02-06 09:56:19 -0600

    Prompt & java config

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
+```bash
+    sudo pacman -S --needed base-devel git
+    git clone https://github.com/fk2731/dotfiles.git
+    ~/.dotfiles/setup.sh
+```
+
+you need to install jdk manually, go [jdk](https://www.oracle.com/mx/java/technologies/downloads/) on Downloads (preffered) and run
+
+```bash
+    tar -xvzf ~/Downloads/jdk-17.0.14_linux-x64_bin.tar.gz -C /opt
+```
+
 To install p10k `git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k`
 `git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions`
 `git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting`
@@ -16,25 +28,22 @@ Open neovim -> `:Mason` -> press 5 -> search for 'stylua' and install it
 
 Open neovim -> `:Mason` -> press 5 -> search for 'black' and install it (for python)
 
-
-
 `sudo pacman -S ttf-font-awesome` for waybar fonts
 
+### bluetooth
 
-
-### bluetooth 
 Install `sudo pacman -S bluez bluez-utils`
 `sudo systemctl start bluetooth`
 `sudo systemctl enable bluetooth`
 Do `bluetoothctl` and then this secuence
--  `power on`
--  `agent on`
--  `default-agent`
--  Identify your device
+
+- `power on`
+- `agent on`
+- `default-agent`
+- Identify your device
 
 ### Wifi
+
 - `nmcli d wifi`
 - identify your network
 - `nmcli d wifi conection <name_of_your_network> password <your_password>`
-
-
--- setup.sh
+++ setup.sh
@@ -2,14 +2,16 @@
 
 set -e  # Exit immediately if a command exits with a non-zero status
 
+cd ~/.dotfiles
+
 # Update pacman configuration
 sudo tee /etc/pacman.conf < pacman.conf > /dev/null
 
 # Install packages with pacman
-sudo pacman -S --needed waybar hyprland kitty ripgrep hyprcursor hypridle hyprlock hyprpaper tldr stow wl-clipboard bat ly npm unzip system-config-printer neovim --noconfirm
+sudo pacman -S lsd waybar hyprland kitty ripgrep hyprcursor hypridle hyprlock hyprpaper tldr stow wl-clipboard bat ly npm unzip system-config-printer neovim wget bluez bluez-utils --noconfirm
 
 # Installation for obs-studio
-sudo pacman -S --needed qt5-wayland xdg-desktop-portal xdg-desktop-portal-hyprland --noconfirm
+sudo pacman -S qt6-wayland xdg-desktop-portal xdg-desktop-portal-hyprland --noconfirm
 
 # Install yay if not installed
 if ! command -v yay &> /dev/null; then
@@ -21,14 +23,28 @@ if ! command -v yay &> /dev/null; then
   rm -rf ~/yay-installer
 fi
 
-# Clone Powerlevel10k theme for Zsh
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+# Install oh-my-zsh
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" --unattended
+
+# Clone Powerlevel11k theme for Zsh
+git clone --depth=2 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 # Install packages with yay
-yay -S --needed hyprshot cava telegram-desktop swaync-git spotify brave-bin zsh-autosuggestions zsh-syntax-highlighting --noconfirm
+yay -S hyprshot cava telegram-desktop swaync-git spotify brave-bin --noconfirm
+
+# Set Zsh as the default shell
+if [ "$SHELL" != "$(which zsh)" ]; then
+  echo "Setting Zsh as the default shell..."
+  chsh -s "$(which zsh)"
+fi
 
 # Apply dotfiles configurations using stow
-cd ~/.dotfiles && stow config
+stow config
+
+# Set theme for bat
+bat cache --build
 
 # Update Ly configuration
 sudo tee /etc/ly/config.ini < config.ini > /dev/null
